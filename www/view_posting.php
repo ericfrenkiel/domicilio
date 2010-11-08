81eb6954348d820ea5804dc96d77631de787279c
Eric <ericfrenkiel@gmail.com>
2010-11-08 10:20:24 +0300

    adding in more structure to view_posting.php

--- www/view_posting.php
+++ www/view_posting.php
@@ -1,7 +1,8 @@
 <?php require_once('../lib/header.php');?>
 <script>
 	$(function() {
-		$( "#tabs" ).tabs();
+		$("#tabs").tabs();
+		$("#tabs2").tabs();
 	});
 </script>
 
@@ -23,6 +24,43 @@
 </div>
 <div id="tabs-4">
 <p>street view</p>
+</div>
+</div>
+<div id="tabs2">
+<ul>
+	<li><a href="#tabs-5">Description</a></li>
+	<li><a href="#tabs-6">Amenities</a></li>
+</ul>
+<div id="tabs-5">
+Designed by E. Stewart Williams in 1947 for Frank Sinatra and his first wife, Nancy Barbato, The Twin Palms Estate is a spectacular example of mid-century architecture in the heart of Palm Springs. This estate boasts both mid-century style luxury and historical significance with modern amenities. This Palm Springs architectural gem features exquisite period furnishings and infamous piano-shaped swimming pool. Williams also has many other significant projects in the Palm Springs area.
+
+The legendary residence was host to some of the valley’s most glamorous parties with Hollywood’s brightest stars of the day. The twin palm trees located poolside, appropriately name the infamous estate. Frank Sinatra often hoisted his Jack Daniels emblem flag on the flag pole once positioned between the twin palms, which indicated an invite to his Movie Colony neighborhood cronies that revelry and cocktails were in order. The home was also witness to some of Sinatra’s most memorable bouts. One of the most outrageous was when Frank threw then-wife Ava Gardner’s possessions onto the driveway after she appeared at the house in an attempt to catch him with Lana Turner. You can still see the chip in the sink where Frank threw a bottle in a rage. The residence has been completely restored with vintage designer furnishings throughout the residence and includes the original sound and recording system installed by Valentino Electronics in Hollywood. The kitchen is the most recent restoration and sponsored by Viking with St Charles modern cabinetry resembling the origin kitchen design. The powder room is sponsored by Kohler.
+
+The home today is perfect for vacationing or special events. The residence includes a master suite, guest suite, guest bedroom, and an exterior entrance guest room. Each bedroom has an ensuite bathroom with an additional powder room in the great room. The kitchen is updated with today modern appliances and perfect for creating culinary art and entertaining. The estate is also wired with the latest technology, including wireless internet and iPod connectivity with surround sound. Guest can enjoy listening to nostalgic Sinatra tunes that are available for enjoyment. While the home does include the original recording equipment, today the cabinet is also used to showcase a flat screen television. Memorabilia can be seen throughout the home in this preserve architectural residence that was once Frank Sinatra’s desert retreat from Hollywood.
+
+Highlights Include:
+-Four bedrooms each with ensuite bathrooms, additional power room is available in the great room
+-Pool house with kitchenette, and his and hers cabana-style bathrooms
+-Sinatra’s original recording studio still in place (although not connected for use)
+-Massive sliding glass doors helps integrate the indoors to the outdoors from the great room opening onto expansive patio and the piano-shaped swimming
+-Exquisitely furnished residence with period pieces and extensive Sinatra memorabilia and art throughout the estate
+-Located in a Central Palm Springs near the Convention Center and Downtown
+-Twin Palms is located just around the corner from residences once owned by Al Jolson, Jack Benny and Cary Grant in the famous Movie Colony neighborhood
+
+-Master Bedroom: King Bed, fireplace, ensuite bathroom with shower-tub and dual vanity, pool access from bedroom, TV
+-Guest Bedroom #1: Queen Bed, ensuite bathroom with shower only, TV
+-Guest Bedroom #2: Queen Bed, fireplace, bathroom with shower only, patio access from bedroom, TV
+-Exterior entrance bedroom near kitchen: Queen Bed, ensuite bathroom with shower only, TV
+-Two poolside bathrooms each with showers; additional powder room off great room
+-Air Conditioning and wireless internet available
+-Airport: Palm Springs Int’l – 3 Miles
+-Shopping: Less than 1 mile
+
+-Daily Rate: $2,600.00 / night*11.5 % occupancy tax
+-$350.00 Service-Amenity Fee - Three Night Minimum Required (additional nights may be required during some Holidays and Popular-Local Calendar Events); Rates inclusive of Departure Cleaning and Standard Pool Heating
+
+</div>
+<div id="tabs-6">
 </div>
 </div>
 <?php require_once('../lib/footer.php');?>
