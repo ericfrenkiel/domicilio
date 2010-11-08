15c55894236d50d4cd05b330f15222f5ea834c62
Eric <ericfrenkiel@gmail.com>
2010-11-08 08:22:07 +0300

    header is back

---
+++ lib/header.php
@@ -0,0 +1,46 @@
+<?php
+require_once("../lib/core.php");
+require_once("../lib/db.php");
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+        <head>
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+                <meta name="title" content="Domicilio" />
+<meta name="description" content="Domicilio is a social layer for real estate." />
+                <title>Domicilio: Social Real Estate</title>
+                <link href="/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+        <script type="text/javascript" src="/js/jquery.js"></script>
+                <script type="text/javascript" src="/js/jquery.maskedinput.js"></script>
+                <script type="text/javascript"
+src="/js/jquery-ui-1.7.2.custom.min.js"></script>
+                <script type="text/javascript" src="/js/jquery.labelify.js"></script>
+                <script type="text/javascript"
+src="/js/jquery.infinitecarousel2.min.js"></script>
+                <link rel="stylesheet" type="text/css" media="screen" href="/css/main.css" />
+                <link rel="stylesheet" type="text/css" media="screen" href="/css/jquery.css" />
+               <link href="/css/facebox.css" media="screen" rel="stylesheet" type="text/css"/>
+<script src="/js/jquery.fbox.js" type="text/javascript"></script>
+</head>
+        <body>
+                <div id="topBar">
+                </div>
+                <div id="container">
+                  <div id="header">
+                        <div id="navigation">
+                          <ol>
+                                <li><a href="/#">Home</a></li>
+                                <li><a href="/#">Blog</a></li>
+                                <li><a href="/#">About</a></li>
+                                <li><a href="/#">Help</a></li>
+                                <li><a href="/#"><b>Sign Up</b></a></li>
+                          </ol>
+                        </div>
+                        <a href="/"><img src="/images/logo.png" alt="Domicilio" border="0"
+/></a>
+                </div>
+                <div id="subnav">
+                </div>
+                  <div id="main">
