b9f6c7a5c57169b767aa1aaaf8d0e2691d85b855
nikita <nikita.shamgunov@gmail.com>
2010-11-08 10:41:09 +0300

    Added the first version of a typeahead

--- www/index.php
+++ www/index.php
@@ -1,7 +1,15 @@
+<html>
+<head>
+	<script type="text/javascript" src="/js/jquery.js"></script>
+	<script type='text/javascript' src='/js/jquery.autocomplete.js'></script>
+	<link rel="stylesheet" type="text/css" href="/css/jquery.autocomplete.css" />	
+</head>
 
 <form action="index.php">
 <input name="search" type="text"/>
-<input name="submit" type="submit"/>
+		<input type="text" id="CityLocal" value="" />
+		<input type="button" value="Get Value" onclick="lookupLocal();" />
+		<input name="submit" type="submit"/>
 </form>
 <?php
 	$LISTINGS_PER_PAGE = 100;
@@ -23,3 +31,66 @@
 	}
 	mysql_close();
 ?>
+
+
+<script type="text/javascript">
+
+var ac = $("#CityLocal").autocomplete("/location_typeahead.php");
+
+function findValue(li) {
+	if( li == null ) return alert("No match!");
+
+	// if coming from an AJAX call, let's use the CityId as the value
+	if( !!li.extra ) var sValue = li.extra[0];
+
+	// otherwise, let's just display the value in the text box
+	else var sValue = li.selectValue;
+
+	alert("The value you selected was: " + sValue);
+}
+
+function selectItem(li) {
+	findValue(li);
+}
+
+function formatItem(row) {
+	return row[0] + " (id: " + row[1] + ")";
+}
+
+function lookupAjax(){
+	var oSuggest = $("#CityAjax")[0].autocompleter;
+
+	oSuggest.findValue();
+
+	return false;
+}
+
+function lookupLocal(){
+	var oSuggest = $("#CityLocal")[0].autocompleter;
+
+	oSuggest.findValue();
+
+	return false;
+}
+
+$(document).ready(function() {
+	$("#CityAjax").autocomplete(
+		"autocomplete_ajax.cfm",
+		{
+			delay:10,
+			minChars:2,
+			matchSubset:1,
+			matchContains:1,
+			cacheLength:10,
+			onItemSelect:selectItem,
+			onFindValue:findValue,
+			formatItem:formatItem,
+			autoFill:true
+		}
+	);
+}
+);
+
+</script>
+
+</html>
