2a04d2abe5fd1947d0765fde8859628a3196a806
nikita <nikita.shamgunov@gmail.com>
2010-11-08 07:04:12 +0300

    Populate data into the table and remove filtering in listings.php

--- www/index.php
+++ www/index.php
@@ -4,7 +4,7 @@
 	mysql_select_db('thedom_info', $link);
 	// Get the list of listings for this user (no more than 100
 	//var_dump($_GET);
-	$query = "select * from postings where facebook_id=".mysql_real_escape_string($_GET['fb_sig_user']);
+	$query = "select * from postings"; // where facebook_id=".mysql_real_escape_string($_GET['fb_sig_user']);
 	$result= mysql_query( $query );
 	if (!$result) {
     		$message  = 'Invalid query: ' . mysql_error() . "\n";
