a917f09d534fd1eaf544c3e37b742bdc57c2a553
Eric <ericfrenkiel@gmail.com>
2010-11-08 08:14:21 +0300

    adding the view state

---
+++ www/view_posting.php
@@ -0,0 +1,16 @@
+<?php require_once('../lib/header.php');?>
+
+<div id="basicinfo">
+basic info
+</div>
+<div id="photos">
+photos
+</div>
+<div id="maps">
+maps
+</div>
+<div id="streetview">
+street view
+</div>
+<?php require_once('../lib/footer.php');?>
+
