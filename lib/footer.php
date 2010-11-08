30aef1583d8e33766de3d4538ccf7e7d755961c3
Eric <ericfrenkiel@gmail.com>
2010-11-08 06:42:57 +0300

    major additions to header and footer. added css for facebook

--- lib/footer.php
+++ lib/footer.php
@@ -1,6 +1,8 @@
-<?php
+ </div>
+                  <div id="footer">
+                          <div id="footersearch" style="float:left;"><input type="text" 
+value="Find a Property" style="float: right; margin-right: 11px; margin-top: 3px;"></div>
+                </div>
+        </body>
+</html>
 
-echo "</body>";
-echo "</html>";
-
-?>
