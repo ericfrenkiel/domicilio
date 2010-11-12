c41c1c1062b0f09d9b8d60caf0e987607eef99d2
Eric <ericfrenkiel@gmail.com>
2010-11-12 08:27:37 +0300

    form now points to view_listings; added it back to stage

---
+++ www/index.php
@@ -0,0 +1,26 @@
+<?php require_once('../lib/header.php');?>
+<div id="tagline" style="margin: 0px auto; margin-bottom: 30px; margin-top:30px; width: 500px; height: auto;">
+<span style="margin:20px;font-size: 36px;height: 50px;">Find your next apartment.</span>
+</div>
+<div id="bigbar" style="width: 500px; height: 50px;margin:0px auto;">
+<form action="/view_listings.php">
+	<input type="text" id="as-selections-q" value="" class="as-value rounded" style="float:left;height: 28px;"/>
+<input name="submit" type="submit" value="Search" class="v3_button v3_fixed_width" style="-moz-border-radius-bottomleft:0;
+-moz-border-radius-topleft:0;
+-webkit-border-radius-topleft:0;
+-webkit-border-radius-bottomleft:0;
+width:120px;position:relative;top:0x;
+"/>
+<span style="font-size:12px; color: #808080;float:left;">Ex. Hardwood floors, Russian Hill, 1 bd/1ba</span>
+</form>
+</div>
+<div id="homeboxbg">
+<div class="homebox">
+</div>
+<div class=homebox">
+</div>
+<div class="homebox">
+</div>
+</div>
+<?php require_once('../lib/footer.php');?>
+
